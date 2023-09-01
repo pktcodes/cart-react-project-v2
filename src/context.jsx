b54181a32bdd010de630b3fe4040f70effadb098
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-31 23:49:40 -0500

    Global Context

---
+++ src/context.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext } from 'react';
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const greeting = 'hello';
+
+  return (
+    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
